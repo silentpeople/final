b2a18a7b840e7468b46f16e253b9c740481a162e
silentpeople <silentpeople@users.noreply.github.com>
2013-11-14 10:46:31 +0800

    Create menu.php

---
+++ page/menu.php
@@ -0,0 +1,17 @@
+
+<a href="../utama.php">Home</a>
+
+
+
+<a href="register.php">Register</a>
+
+
+<div id="menu"	>
+<div align="center" >
+<a href="./page/add_book.php">Tambah Buku</a> / 
+<a href="page/view_book.php">Senarai Buku</a> / 
+<a href="page/update_book.php">KemasKini Buku</a> /  
+<a href="page/delete_book.php">Lupus Buku</a> /
+
+<a href="logout.php">Logout</a>  
+
