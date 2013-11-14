47487c78010206ceadbcfe015c47613d92ceeb2c
silentpeople <silentpeople@users.noreply.github.com>
2013-11-14 10:47:38 +0800

    utama @ index

---
+++ utama.php
@@ -0,0 +1,32 @@
+<html>
+<title>ApoGee Assignment</title>
+<link rel="stylesheet" href="css/index.css" />
+
+<body>
+<div id="menu">
+<div align="left"><?php
+include 'widgets/date_time.php';
+
+?>
+
+</div>
+</div>
+<div id="menu">
+<?php
+
+
+include 'page/menu.php';//element dlm website
+
+?>
+</div>
+<div id="container">
+<div align="center">
+
+<br><br>
+<h1>SELAMAT DATANG KE SISTEM BUKU </h1>
+<input type="image" width="800" height="400" name="pic1" id="pic1" src="img/magical-book.jpg">
+</div>
+</div>
+
+</body>
+</html>
